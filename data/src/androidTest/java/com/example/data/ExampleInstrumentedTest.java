a6de39d65ccbd5449b7352f3614945b28b48ac89
manuel.lopez <manuel.lopez@globant.com>
2019-05-25 17:34:34 -0500

    update package name

--- data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.java
+++ data/src/androidTest/java/com/example/data/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.angelvargas.data.test", appContext.getPackageName());
+        assertEquals("com.example..data.test", appContext.getPackageName());
     }
 }
--- domain/src/androidTest/java/com/example/myresume/domain/ExampleInstrumentedTest.java
+++ domain/src/androidTest/java/com/example/myresume/domain/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.angelvargas.myresume.domain.test", appContext.getPackageName());
+        assertEquals("com.example.myresume.domain.test", appContext.getPackageName());
     }
 }
