df87abeccb789dfa889be3b75b77e20723c3a2d7
Angel Vargas <angel.vargas@globant.com>
2019-05-25 16:37:48 -0500

    [RESUME] Create MainActivityTests

--- app/src/androidTest/java/com/example/myresume/MainActivityTests.kt
+++ app/src/androidTest/java/com/example/myresume/MainActivityTests.kt
@@ -4,13 +4,18 @@ import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.example.myresume.view.MainActivity
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTests {
 
-
     @Rule
     @JvmField
     var activityRule = ActivityTestRule(MainActivity::class.java, true, false)
+
+    @Test
+    fun testHeaderHasInformation() {
+        
+    }
 }
--- app/src/androidTest/java/com/example/myresume/MockMyResumeApp.kt
+++ app/src/androidTest/java/com/example/myresume/MockMyResumeApp.kt
@@ -1,11 +1,13 @@
 package com.example.myresume
 
 import com.example.myresume.di.AppComponent
-import com.example.myresume.view.MainActivity
 
 class MockMyResumeApp: ResumeApplication() {
 
     override fun createComponent(): AppComponent {
-        return DaggerMock
+        return DaggerMockAppComponent.builder()
+            .mockAppModule(MockAppModule())
+            .mockThreadModule(MockThreadModule())
+            .build() as AppComponent
     }
 }
